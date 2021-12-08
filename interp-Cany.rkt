87c0d12a89052712d975db0fa154acb2fe305c13
Jeremy Siek <jsiek@indiana.edu>
2021-12-08 16:31:40 -0500

    update interp-Cany

--- interp-Cany.rkt
+++ interp-Cany.rkt
@@ -3,17 +3,19 @@
 (require "interp-Lany-prime.rkt")
 (require "interp-Cvar.rkt")
 (require "interp-Cif.rkt")
+(require "interp-Cwhile.rkt")
 (require "interp-Cvec.rkt")
 (require "interp-Cfun.rkt")
 (require "interp-Clambda.rkt")
 (provide interp-Cany)
 
 (define Cany-class (interp-Clambda-mixin
-                  (interp-Cfun-mixin
-                   (interp-Cvec-mixin
-                    (interp-Cif-mixin
-                     (interp-Cvar-mixin
-                      interp-Lany-prime-class))))))
+                    (interp-Cfun-mixin
+                     (interp-Cvec-mixin
+                      (interp-Cwhile-mixin
+                       (interp-Cif-mixin
+                        (interp-Cvar-mixin
+                         interp-Lany-prime-class)))))))
 
 (define (interp-Cany p)
   (send (new Cany-class) interp-program p))
